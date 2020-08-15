11592630cc0d2315031ca9a1b1051a9678a540b5
Max Verevkin <me@maxverevkin.tk>
2020-08-15 12:40:12 +0300

    remove vec2d_t

--- structs.cpp
+++ structs.cpp
@@ -2,26 +2,6 @@
 
 #include <math.h>
 
-// Dot product
-double vec2d_t::operator*(const vec2d_t &vec) const {
-    return x*vec.x + y*vec.y;
-}
-
-vec2d_t vec2d_t::operator*(double a) const {
-    return vec2d_t {x*a, y*a};
-}
-
-vec2d_t vec2d_t::operator-(const vec2d_t &vec) const {
-    return vec2d_t {x-vec.x, y-vec.y};
-}
-vec2d_t vec2d_t::operator+(const vec2d_t &vec) const {
-    return vec2d_t {x+vec.x, y+vec.y};
-}
-
-double vec2d_t::length() const {
-    return sqrt(x*x+y*y);
-}
-
 
 // Dot product
 double vec3d_t::operator*(const vec3d_t &vec) const {
